d09c3587cf60b0953b87ef4a63ab7ea048930af7
Igor <hdd500t@yandex.ru>
2021-08-03 12:25:01 +0300

    task 1 complete

--- task_1-2.py
+++ task_1-2.py
@@ -30,6 +30,24 @@ def add_list_ingredient_to_cook_book(file_name):
             file.readline()
     return cook_book
 
+def get_shop_list_by_dish(cook_book, dish, person_count):
+    dict_ingredients_count_dict = {}
+    for ingredients in cook_book[dish]:
+        ingredients_count_dict = {}
+        ingredients_count_dict['measure'] = ingredients['measure']
+        ingredients_count_int = int(ingredients['quantity']) * person_count
+        ingredients_count_dict['quantity'] = ingredients_count_int
+        dict_ingredients_count_dict[ingredients['ingredient_name']] = ingredients_count_dict
+    return dict_ingredients_count_dict
 
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list_by_dishes = {}
+    cook_book = add_list_ingredient_to_cook_book('recipes.txt')
+    shop_list_by_dishes = {}
+    for dish in dishes:
+        shop_list_by_dishes.update(get_shop_list_by_dish(cook_book, dish, person_count))
+    return shop_list_by_dishes
 
-print(add_list_ingredient_to_cook_book('recipes.txt'))
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+# print(add_list_ingredient_to_cook_book('recipes.txt'))
